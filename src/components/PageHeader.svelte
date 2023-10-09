b6ebd1321b4b1162e519bd2285b8f607c00be32b
gigor <i@gigor.ru>
2023-10-09 18:49:51 +0400

    Update PageHeader.svelte

--- src/components/PageHeader.svelte
+++ src/components/PageHeader.svelte
@@ -1,11 +1,12 @@
 
-
-<h1 class="p-4 text-xl font-black tracking-wider bg-white border-b w-full">
+<div class="p-4  bg-white border-b w-full flex flex-col gap-4 md:flex-row justify-between items-center">
+<h1 class=" text-xl font-black tracking-wider">
     <img src='/favicon.png' alt="Logo" class="w-8 h-8 inline-block align-text-bottom"/>
     UTM Link Builder
 </h1>
 
 
 
+<a href="https://www.producthunt.com/posts/utm-links?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-utm&#0045;links" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=418967&theme=light" alt="UTM&#0032;Links - Create&#0032;&#0038;&#0032;store&#0032;your&#0032;UTM&#0032;links | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
 
-
+</div>
